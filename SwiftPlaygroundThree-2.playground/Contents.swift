ac46df29a8a0437c1b061085ad3bdbc615fbb248
Sam Smith <messersmithx@gmail.com>
2020-02-18 10:28:06 -1000

    Completed!

---
+++ SwiftPlaygroundThree-2.playground/Contents.swift
@@ -0,0 +1,41 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var fruitNames: [String] = []
+
+fruitNames = ["SuperFruit", "MasterFruit", "UltiFruit"]
+
+
+
+
+print(fruitNames)
+
+print("I like to eat \(fruitNames)")
+
+for theFruitIAmGoingToEat in fruitNames {
+    print("I like to eat " + theFruitIAmGoingToEat)
+}
+
+print(fruitNames[0])
+
+
+
+
+
+var thisArrayisCool : [String] = []
+thisArrayisCool = ["Mercedes", "Koenigsegg", "Lamborghini", "Bugatti", "Ashton Marton", "Ferrari", "Bmw", "Toyota"]
+
+for nameOfCar in thisArrayisCool {
+    print(nameOfCar + " is a very expensive car!")
+}
+
+
+
+var butThisArrayIsCooler : [integer_t] = [100000, 200000, 300000, 400000, 500000, 600000, 700000, 800000]
+
+for bigNumbers in butThisArrayIsCooler {
+    print(bigNumbers + 100)
+}
+
+
